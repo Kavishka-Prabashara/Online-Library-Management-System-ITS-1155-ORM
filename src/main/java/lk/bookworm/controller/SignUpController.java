a553b8b12df468ad71da61ff7c2f546e2d71fec1
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-12 23:37:06 +0530

    signUp Form is created.

---
+++ src/main/java/lk/bookworm/controller/SignUpController.java
@@ -0,0 +1,36 @@
+package lk.bookworm.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+
+public class SignUpController  {
+
+    @FXML
+    private Hyperlink btnLogin;
+
+    @FXML
+    private JFXButton btnSignUp;
+
+    @FXML
+    private TextField getUserEmail;
+
+    @FXML
+    private TextField getUserName;
+
+    @FXML
+    private TextField getUserPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+}
