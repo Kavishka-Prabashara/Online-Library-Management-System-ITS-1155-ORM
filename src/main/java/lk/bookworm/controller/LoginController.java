4ed902532222a55c202f402811290c6cd9a82ec2
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-12 19:08:23 +0530

    Created login controller.

---
+++ src/main/java/lk/bookworm/controller/LoginController.java
@@ -0,0 +1,24 @@
+package lk.bookworm.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
